2e6f65c83e13e3e30658f59b97c01eeb5d8bdfe3
udit singh <jarves.pro@gmail.com>
2024-11-27 02:25:51 +0530

    Add files via upload

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false
+		})
+	},
+	alias: {
+		"@/*": "./src/*"
+	}
+};
+
+export default config;
